4c043fbdf55b8b096fd3a35527c2f4ac2c85706e
Kenneth Spatta <kenyounot@gmail.com>
2020-02-02 18:33:27 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import BandsContainer from './containers/BandsContainer';
+import manageBand from './reducers/manageBand';
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore(manageBand);
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <BandsContainer />
-      </div>
-    );
-  }
-};
+	render() {
+		return (
+			<Provider store={store}>
+				<div className='App'>
+					<BandsContainer />
+				</div>
+			</Provider>
+		);
+	}
+}
 
 export default App;
